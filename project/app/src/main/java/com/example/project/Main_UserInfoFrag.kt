495a3b4e0276f2238a60c01d5940c06c9c6fbd09
Kelan Albertson <kelan.albertson@utah.edu>
2022-09-07 14:39:30 -0600

    added onClick to Edit Profile Button to go from Main to Profile

--- project/app/src/main/java/com/example/project/Main_UserInfoFrag.kt
+++ project/app/src/main/java/com/example/project/Main_UserInfoFrag.kt
@@ -1,10 +1,12 @@
 package com.example.project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class Main_UserInfoFrag : Fragment() {
 
@@ -18,6 +20,16 @@ class Main_UserInfoFrag : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_userinfo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnEditProfile: Button = view.findViewById(R.id.btnEditProfile)
+
+        btnEditProfile.setOnClickListener{
+            val profileIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(profileIntent)
+        }
     }
 
 }
