0a6ad6a174c783fab307ca35316333a938a86581
Diego <lucy@lucy.uconnect.utah.edu>
2022-09-08 16:29:01 -0600

    more unit tests

--- project/app/src/test/java/com/example/project/BMRTest.kt
+++ project/app/src/test/java/com/example/project/BMRTest.kt
@@ -6,37 +6,73 @@ import org.junit.Assert.*
 
 internal class BMRTest {
 
-@Test
+    @Test
     fun calculateBMRMen() {
         val bmr = BMR()
         val result = bmr.calculateBMRMen(25, 183.0, 80.0)
         assertEquals(1913.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateBMRWomen() {
         val bmr = BMR()
         val result = bmr.calculateBMRWomen(25, 183.0, 80.0)
         assertEquals(1652.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateCaloriesSedentary() {
+        val bmr = BMR()
+        //male
+        var result = bmr.calculateCaloriesSedentary(1913.0)
+        assertEquals(2296.0, result, 10.0)
+        //female
+        result = bmr.calculateCaloriesSedentary(1652.0)
+        assertEquals(1982.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateCaloriesMildActivity() {
+        val bmr = BMR()
+        //male
+        var result = bmr.calculateCaloriesMildActivity(1913.0)
+        assertEquals(2630.0, result, 10.0)
+        //female
+        result = bmr.calculateCaloriesMildActivity(1652.0)
+        assertEquals(2272.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateCaloriesModerateActivity() {
+        val bmr = BMR()
+        //male
+        var result = bmr.calculateCaloriesModerateActivity(1913.0)
+        assertEquals(2965.0, result, 10.0)
+        //female
+        result = bmr.calculateCaloriesModerateActivity(1652.0)
+        assertEquals(2561.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateCaloriesHeavyActivity() {
+        val bmr = BMR()
+        //male
+        var result = bmr.calculateCaloriesModerateActivity(1913.0)
+        assertEquals(3252.0, result, 10.0)
+        //female
+        result = bmr.calculateCaloriesModerateActivity(1652.0)
+        assertEquals(2808.0, result, 10.0)
     }
 
-@Test
+    @Test
     fun calculateCaloriesExtremeActivity() {
+        val bmr = BMR()
+        //male
+        var result = bmr.calculateCaloriesModerateActivity(1913.0)
+        assertEquals(3635.0, result, 10.0)
+        //female
+        result = bmr.calculateCaloriesModerateActivity(1652.0)
+        assertEquals(3139.0, result, 10.0)
+
     }
 }
