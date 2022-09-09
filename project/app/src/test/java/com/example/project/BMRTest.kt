a2f8b79f7cb23ba415e375a47a74aaf45c758bc3
Diego <lucy@lucy.local>
2022-09-09 09:01:52 -0600

    junit test refactoring

--- project/app/src/test/java/com/example/project/BMRTest.kt
+++ project/app/src/test/java/com/example/project/BMRTest.kt
@@ -10,23 +10,21 @@ internal class BMRTest {
     private val dude1 = Triple(25, 183.0, 80.0)
     private val gal1 = Triple(25, 183.0, 80.0)
 
+    private val bmr = BMR()
     @Test
     fun calculateBMRMen() {
-        val bmr = BMR()
         val result = bmr.calculateBMRMen(dude1.first, dude1.second, dude1.third)
         assertEquals(1913.0, result, delta)
     }
 
     @Test
     fun calculateBMRWomen() {
-        val bmr = BMR()
         val result = bmr.calculateBMRWomen(gal1.first, gal1.second, gal1.third)
         assertEquals(1652.0, result, delta)
     }
 
     @Test
     fun calculateCaloriesSedentary() {
-        val bmr = BMR()
         //male
         var result = bmr.calculateCaloriesSedentary(1913.0)
         assertEquals(2296.0, result, delta)
@@ -37,7 +35,6 @@ internal class BMRTest {
 
     @Test
     fun calculateCaloriesMildActivity() {
-        val bmr = BMR()
         //male
         var result = bmr.calculateCaloriesMildActivity(1913.0)
         assertEquals(2630.0, result, delta)
@@ -48,7 +45,6 @@ internal class BMRTest {
 
     @Test
     fun calculateCaloriesModerateActivity() {
-        val bmr = BMR()
         //male
         var result = bmr.calculateCaloriesModerateActivity(1913.0)
         assertEquals(2965.0, result, delta)
@@ -59,7 +55,6 @@ internal class BMRTest {
 
     @Test
     fun calculateCaloriesHeavyActivity() {
-        val bmr = BMR()
         //male
         var result = bmr.calculateCaloriesHeavyActivity(1913.0)
         assertEquals(3252.0, result, delta)
@@ -70,13 +65,11 @@ internal class BMRTest {
 
     @Test
     fun calculateCaloriesExtremeActivity() {
-        val bmr = BMR()
         //male
         var result = bmr.calculateCaloriesExtremeActivity(1913.0)
         assertEquals(3635.0, result, delta)
         //female
         result = bmr.calculateCaloriesExtremeActivity(1652.0)
         assertEquals(3139.0, result, delta)
-
     }
 }
