bcd293febcd7bb85922c55df0f0b8510d9ef6168
Kelan Albertson <kelan.albertson@utah.edu>
2022-09-12 13:33:25 -0600

    implemented edit profile button

--- project/app/src/main/java/com/example/project/MainFrag.kt
+++ project/app/src/main/java/com/example/project/MainFrag.kt
@@ -8,7 +8,6 @@ import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,10 +17,12 @@ import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
+
 
 @SuppressLint("MissingPermission")
 class MainFrag : Fragment() {
@@ -54,6 +55,10 @@ class MainFrag : Fragment() {
 
         btnEditProfile.setOnClickListener{
             //TODO switch the fragment
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.frag_container, ProfileFrag(), "Profile Fragment")
+            transaction.addToBackStack(null)
+            transaction.commit()
             Log.d("MainFrag", "onCreateView: edit profile button clicked")
         }
 
