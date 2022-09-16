5f3a999c3a341329ef984af171ead45808f3e378
Kelan Albertson <kelan.albertson@utah.edu>
2022-09-16 15:35:54 -0600

    added functionality to location button on profile page

--- project/app/src/main/java/com/example/project/ProfileFrag.kt
+++ project/app/src/main/java/com/example/project/ProfileFrag.kt
@@ -1,5 +1,6 @@
 package com.example.project
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.ActivityNotFoundException
@@ -7,15 +8,24 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import java.io.File
 import java.io.FileOutputStream
 import java.text.SimpleDateFormat
@@ -26,6 +36,10 @@ class ProfileFrag : Fragment() {
 
     private var mProfilePicPath : String? = null
 
+    private var mLatitude = 0.0
+    private var mLongitude = 0.0
+
+    @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -58,14 +72,47 @@ class ProfileFrag : Fragment() {
         arrayAdapter.setDropDownViewResource(R.layout.spinner_list_profile)
         spActivityLvl.adapter = arrayAdapter
 
-
-        // TODO -- use this in listener to get radio button value (it is returned as an Int)
         val rgSex: RadioGroup = view.findViewById(R.id.radio_sex)
 
-        // TODO -- use this to change value of numberpicker stored in member variable
-//                np.setOnValueChangedListener { picker, oldVal, newVal ->
-//            val text = "Changed from $oldVal to $newVal"
-//        }
+        // TODO
+        // get the location TextView
+        val tvLocation: TextView = view.findViewById(R.id.tvLocation)
+
+        // get the location button
+        val btnLocation: Button = view.findViewById(R.id.btnLocation)
+        // add the click functionality
+        btnLocation.setOnClickListener {
+
+            // get current GPS location
+            val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(view.context)
+
+            val appPerms = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            Log.d("MainFrag", "onViewCreated: appPerms: $appPerms")
+            activityResultLauncher.launch(appPerms)
+
+            val priority = Priority.PRIORITY_BALANCED_POWER_ACCURACY
+            val cancellationTokenSource = CancellationTokenSource()
+
+            fusedLocationClient.getCurrentLocation(priority, cancellationTokenSource.token)
+                .addOnSuccessListener { location: Location? ->
+                    // getting the last known or current location
+                    mLatitude = location!!.latitude
+                    mLongitude = location.longitude
+
+                    Toast.makeText(activity, "Latitude:$mLatitude\nLongitude:$mLongitude", Toast.LENGTH_SHORT).show()
+
+                    val address = getAddress(mLatitude, mLongitude)
+
+                    tvLocation.text = address
+                }
+                .addOnFailureListener {
+                    Toast.makeText(activity, "Failed on getting current location", Toast.LENGTH_SHORT).show()
+                }
+
+        }
 
         // Get the SharedPreferences to read from/store in
         val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
@@ -106,6 +153,9 @@ class ProfileFrag : Fragment() {
             val radioFemale: RadioButton = view.findViewById(R.id.radio_female)
             radioFemale.isChecked = !sharedPref.getBoolean("isMale", true)
 
+            // Get the location
+            tvLocation.text = sharedPref.getString("location", "Location")
+
             // Get the profile pic
             val profilePicView : ImageView = view.findViewById(R.id.profilePic)
             mProfilePicPath = sharedPref.getString("profilePic", "")
@@ -141,6 +191,7 @@ class ProfileFrag : Fragment() {
                 val radioButton: RadioButton = view.findViewById(rgSex.checkedRadioButtonId)
                 // store a boolean (for less space + ease) representing whether they are male or not
                 putBoolean("isMale", radioButton.text.toString() == "Male")
+                putString("location", tvLocation.text.toString())
                 putString("profilePic", mProfilePicPath)
 
                 putBoolean("hasProfile", true)
@@ -202,5 +253,26 @@ class ProfileFrag : Fragment() {
             val state = Environment.getExternalStorageState()
             return Environment.MEDIA_MOUNTED == state
         }
+
+    // adapted from https://stackoverflow.com/questions/40760625/how-to-check-permission-in-fragment
+    private var activityResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { result ->
+            var allAreGranted = true
+            for(b in result.values) {
+                allAreGranted = allAreGranted && b
+            }
+
+            if(allAreGranted) {
+
+            }
+        }
+
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geocoder = Geocoder(context)
+        val list = geocoder.getFromLocation(lat, lng, 1)
+//        return list[0].getAddressLine(0)
+        return list[0].locality + ", " + list[0].adminArea + ", " + list[0].countryName
+    }
 
 }
