59558d77204222c9a8d716127365c59e6dcf10a4
Jon <jon.hughes.msd@gmail.com>
2022-09-15 20:12:08 -0600

    successful weather api call

---
+++ project/app/src/main/java/com/example/project/NetworkUtils.kt
@@ -0,0 +1,46 @@
+package com.example.project
+
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+object NetworkUtils {
+    //api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid=37f67a96d9d9b030b54b6ad18b69d26e
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat="
+    private const val LONQUERY = "&lon="
+    private const val APPIDQUERY = "&appid="
+    private const val app_id = "37f67a96d9d9b030b54b6ad18b69d26e"
+
+    fun buildURLFromString(lat: Double, lon: Double): URL? {
+        var myURL : URL? = null
+        try {
+            myURL = URL(BASE_URL + lat + LONQUERY + lon + APPIDQUERY + app_id)
+        } catch (e: MalformedURLException){
+            e.printStackTrace()
+        }
+        return myURL
+    }
+
+    fun getDataFromURL(url: URL): String? {
+        val urlConnection = url.openConnection() as HttpURLConnection
+        return try {
+            val inputStream = urlConnection.getInputStream()
+
+            val scanner = Scanner(inputStream)
+            scanner.useDelimiter("\\A")
+            val hasInput = scanner.hasNext()
+            if (hasInput){
+                scanner.next()
+            }
+            else {
+                null
+            }
+        }
+        finally {
+            urlConnection.disconnect()
+        }
+
+    }
+
+}
