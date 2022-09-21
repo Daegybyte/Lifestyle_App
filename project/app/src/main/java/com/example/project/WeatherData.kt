9d952e9db5ef4252dba9a74c45785a41c8b5613f
Jon <jon.hughes.msd@gmail.com>
2022-09-21 12:34:13 -0600

    deleted unused weatherdata.kt

--- project/app/src/main/java/com/example/project/WeatherData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.project
-
-data class WeatherData(val weather: List<Weather>, val sys: List<Sys>, val name: Name){
-    data class Weather(val main: String, val description: String, val icon: String)
-    data class Sys(val country: String)
-    data class Name(val name: String)
-}
