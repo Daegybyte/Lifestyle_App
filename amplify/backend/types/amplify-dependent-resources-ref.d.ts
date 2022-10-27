9d9c8671a6dee7e5beaff9a01740e1f4667e88f2
Diego <lucy@lucy.uconnect.utah.edu>
2022-10-27 12:54:44 -0600

    added some amplify stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,21 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "msdlifestyleappd525f96d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "mrresource": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
