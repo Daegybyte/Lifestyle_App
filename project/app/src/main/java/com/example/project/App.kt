f40f1de9af1e07f6ac426d7ce856c09968e5623b
Diego <lucy@lucy.uconnect.utah.edu>
2022-11-02 12:41:27 -0600

    commented app

--- project/app/src/main/java/com/example/project/App.kt
+++ project/app/src/main/java/com/example/project/App.kt
@@ -4,10 +4,24 @@ import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
+/**
+ * The application class for the app. This class is used to initialize the database and the repository.
+ * @see application the application context
+ * @see [Application]
+ * @see [UserDatabase]
+ */
 class App : Application() {
-
+    // coroutine scope for the app
     val applicationScope = CoroutineScope(SupervisorJob())
-    val database by lazy { RoomDB.getDatabase(this, applicationScope) }
-    val repository by lazy { SharedRepository.getInstance(database.UserDao(), database.DBWeatherDao(), applicationScope) }
+
+    // lazy initialization of the database
+    val database by lazy {
+        RoomDB.getDatabase(this, applicationScope)
+    }
+
+    // lazy initialization of the repository
+    val repository by lazy {
+        SharedRepository.getInstance(database.UserDao(), database.DBWeatherDao(), applicationScope)
+    }
 
 }
