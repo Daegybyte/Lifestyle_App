3f25c37c31a468049b114f34052042962c24d700
Jon <jon.hughes.msd@gmail.com>
2022-11-02 12:38:03 -0600

    clean up and comments

--- project/app/src/main/java/com/example/project/UserDao.kt
+++ project/app/src/main/java/com/example/project/UserDao.kt
@@ -4,12 +4,11 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)    // replace allows us to update individual values
     suspend fun insert(user: User)
 
     @Query("DELETE FROM user_table")
